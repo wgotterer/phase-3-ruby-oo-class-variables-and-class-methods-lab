db9328403a913f7be93519b1d01a46c1a69e8cba
William Gotterer <william.gotterer@gmail.com>
2021-11-03 09:23:58 -0400

    commit tree 5046896e5a84b08fb6662a18bd290929376b25c2

---
+++ lib/song.rb
@@ -0,0 +1,45 @@
+require 'pry'
+
+class Song
+
+    @@count = 0
+    @@artists = []
+    @@genres = []
+   
+
+    attr_accessor :name, :artist, :genre
+
+    def initialize(name, artist, genre)
+        @@count += 1
+        @name = name
+        @artist = artist
+        @@artists << artist
+        @genre = genre
+        @@genres << genre
+    end
+
+    def self.count
+        @@count
+    end
+
+    def self.genres
+        @@genres.uniq
+    end
+
+    def self.artists
+        @@artists.uniq
+    end
+
+    def self.genre_count
+
+            @@genres.tally
+
+    end
+
+    def self.artist_count
+
+        @@artists.tally
+    end
+
+
+end
